4416587d82b077e1f0eda0c6c7c2c6d4320cb281
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2024-02-15 11:44:29 +0100

    add inputs

--- src/components/Filter/FilterCars.jsx
+++ src/components/Filter/FilterCars.jsx
@@ -4,10 +4,20 @@ import { StyledContainer } from './FilterCars.styled';
 export const FilterCars = () => {
   return (
     <StyledContainer>
-      <p>Car brand</p>
-      <p>Price / 1 hour</p>
-      <p>Car mileage / km</p>
-      <button type='button'>Search</button>
+      <label>
+        <p>Car brand</p>
+        <input />
+      </label>
+      <label>
+        <p>Price / 1 hour</p>
+        <input />
+      </label>
+      <label>
+        {' '}
+        <p>Car mileage / km</p>
+        <input />
+      </label>
+      <button type="button">Search</button>
     </StyledContainer>
   );
 };
